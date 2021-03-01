31ec3dc1193542cb428372bda3a02a64db49d968
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-03-01 11:55:05 +0100

    Project import not working properly. Some issue with dependency

---
+++ src/main/java/se/lexicon/testin_setup/TestinSetupApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.testin_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestinSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestinSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/testin_setup/TestinSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.testin_setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestinSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
